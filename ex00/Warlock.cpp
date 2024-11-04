b460c4b1ca04e92e60d8a7a6b555929de42e7395
BakytHack <40731866+bakyt92@users.noreply.github.com>
2024-11-04 13:09:32 +0300

    Update Warlock.cpp

--- ex00/Warlock.cpp
+++ ex00/Warlock.cpp
@@ -1,44 +1,49 @@
 #include "Warlock.hpp"
 
-const std::string& Warlock::getName(void) const
-{
-    return (this->name);
-}
-
-const std::string& Warlock::getTitle(void) const
-{
-    return (this->title);
-}
-
-void Warlock::setTitle (const std::string &_title)
-{
-    this->title = _title;
-    return;
-}
-
-void Warlock::introduce() const
-{
-    std::cout << getName() << ": I am " << getName() << ", " << getTitle() << '!' << std::endl;
-}
-
-
-Warlock::Warlock(std::string _name, std::string _title): name(_name), title(_title)
-{
-    introduce();
-}
-Warlock::Warlock(const Warlock &rhs)
-{
-    *this = rhs; 
-}
+    Warlock::Warlock(std::string _name, std::string _title) : name(_name), title(_title)
+    {
+        std::cout << this->getName() << ": This looks like another boring day." << std::endl;
+    }  
     
-Warlock& Warlock::operator=(const Warlock &rhs)
-{
-    this->title = rhs.name;
-    this->name = rhs.name;
-    return(*this);
-}    
+    Warlock::Warlock(const Warlock &src)
+    {
+        this->name = src.name;
+        this->title = src.title;
+        *this = src;
+    }
+
+    Warlock& Warlock::operator=(const Warlock &rhs)
+    {
+        if (this != &rhs)
+        {
+            this->name = rhs.name;
+            this->title = rhs.title;
+        }
+        return *this;
+    }
+
+    const std::string& Warlock::getName(void) const
+    {
+        return (this->name);
+    }
     
-Warlock::~Warlock()
-{
-    std::cout << getName() << ": Ny job here is done!" << std::endl;
-}
+    const std::string& Warlock::getTitle(void) const
+    {
+        return (this->title);
+    }
+
+    void Warlock::introduce() const
+    {
+        std::cout << this->getName() << ": I am " << this->getName() << ", " << this->getTitle() << "!" << std::endl;
+    }
+
+    void Warlock::setTitle(const std::string &newTitle)
+    {
+        this->title = newTitle;
+        return;
+    }
+
+    Warlock::~Warlock()
+    {
+        std::cout << this->getName() << ": My job here is done!" << std::endl;
+    } 
